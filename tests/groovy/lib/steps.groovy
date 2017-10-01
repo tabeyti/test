2700e12dd00fbc15936e36b9cedc3db42a3d0efd
tabeyti <tabeyti@gmail.com>
2017-09-30 19:45:00 -0700

    More stuff.

--- tests/groovy/lib/steps.groovy
+++ tests/groovy/lib/steps.groovy
@@ -1,35 +1,29 @@
 package tests.lib
 
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.rules.TemporaryFolder
-import org.junit.runner.RunWith 
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameter
-import org.junit.runners.Parameterized.Parameters
-import testSupport.LibTestBase
-import com.lesfurets.jenkins.unit.BasePipelineTest
+import tests.testSupport.PipelineTestBase
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import hudson.model.Result
 
 /**
  * Tests shared vars 'step's.
  */
-class StepTests extends BasePipelineTest  {
+class StepTests extends PipelineTestBase  {
 
-  @Override
-  @Before
-  void setUp() throws Exception {
-    scriptRoots += 'var'
-    scriptRoots += 'src/org/classic'
-    super.setUp()
-    binding.setVariable('scm', [branch: 'master'])
-  }
+  void "tests the nodex step"() {
+    given:
+        WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 
-  @Test
-  void pkill_smoke() throws Exception {
-    println "----------------------------------------"
-    println "HEY YOU"
-    println "----------------------------------------"
-    sleep(1000)
+    when:
+      p.setDefinition(new CpsFlowDefinition("""
+            
+            nodex("master"){ 
+                println 'IN A NODEX BLOCK!!'
+            }"""
+        ));
+
+    then:
+    println ""
+    j.assertBuildStatusSuccess(p.scheduleBuild2(0));
   }
 }
---
+++ tests/groovy/testSupport/PipelineTestBase.groovy
@@ -0,0 +1,36 @@
+package tests.testSupport
+
+import org.jvnet.hudson.test.JenkinsRule;
+import hudson.model.*;
+import org.junit.Rule
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import spock.lang.Specification
+import jenkins.plugins.git.GitSCMSource;
+import org.jenkinsci.plugins.workflow.libs.*
+/**
+ * A base class for Spock testing using the pipeline helper
+ */
+class PipelineTestBase extends Specification {
+
+    @Rule JenkinsRule j = new JenkinsRule()
+
+    /**
+     * Do the common setup
+     */
+    def setup() {
+
+        def lib = new LibraryConfiguration("rsg", new SCMSourceRetriever(new GitSCMSource(null, System.getProperty("user.dir"), "", "*", "0", true)))
+        List<String> gitHeadNameCmd = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+        def pb = new ProcessBuilder(gitHeadNameCmd).start()
+        pb.consumeProcessErrorStream(System.err)
+        pb.waitFor();
+        if (pb.exitValue() != 0) {
+            throw new RuntimeException("Error running "+gitHeadNameCmd.join(" ")+". The test must run from a git repository")
+        }
+        lib.setImplicit(true)
+        lib.setDefaultVersion(pb.text.trim())
+        GlobalLibraries.get().setLibraries(Collections.singletonList(lib));
+    }
+}
